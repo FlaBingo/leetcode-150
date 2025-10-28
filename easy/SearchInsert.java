a5887d6ea4fb9cd7a43fa2074c3a1c3083b4fbad
Satyam Prajapati <satyamm1402@gmail.com>
2025-10-28 16:38:48 +0530

    Add SearchInsert class with mySolution method to find the insert position in a sorted array

---
+++ easy/SearchInsert.java
@@ -0,0 +1,43 @@
+package easy;
+
+// 35. Search Insert Position
+
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+
+// Example 2:
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+
+// Example 3:
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+public class SearchInsert {
+  public static void main(String[] args) {
+    int[] nums = { 1, 3, 5, 6 };
+    System.out.println(mySolution(nums, 8));
+  }
+
+  static int mySolution(int[] nums, int target) {
+    int start = 0, end = nums.length - 1;
+
+    while (start <= end) {
+      int mid = start + (end - start) / 2;
+
+      if (target == nums[mid]) {
+        return mid;
+      } else if (target > nums[mid]) {
+        start = mid + 1;
+      } else {
+        end = mid - 1;
+      }
+    }
+    return start;
+  }
+}
